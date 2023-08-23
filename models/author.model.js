83797920210b001303202b2322e3637e4adae472
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-24 02:49:19 +0300

    feat: create an author model

---
+++ models/author.model.js
@@ -0,0 +1,39 @@
+import sequelize from "../database/index.database.js";
+import { DataTypes } from "sequelize";
+
+const Author = sequelize.define(
+  "Author",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      type: DataTypes.TEXT,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "authors",
+  }
+);
+
+export default Author;
