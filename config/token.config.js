bf637de12bcae178201258c819cdf20e0c7d9474
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-24 02:19:05 +0300

    feat: add new constants

--- config/token.config.js
+++ config/token.config.js
@@ -1,3 +1,8 @@
 import "dotenv/config";
 
 export const { ACCESS_SECRET_TOKEN, REFRESH_SECRET_TOKEN } = process.env;
+export const EXPIRES_IN_REDIS = 365 * 24 * 60 * 60;
+export const EXPIRES_IN_OPTIONS = {
+  ACCESS_TOKEN: "15m",
+  REFRESH_TOKEN: "180d",
+};
