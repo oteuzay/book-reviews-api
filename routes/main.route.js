8aa510dfd025bc23593da4dc7a7836e80823a30f
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-24 03:20:08 +0300

    feat: implement swagger to main

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoute from "./auth.route.js";
+import swaggerRoute from "./swagger.route.js";
 
 const router = Router();
 
+router.use("/api-docs", swaggerRoute);
 router.use("/auth", authRoute);
 
 export default router;
