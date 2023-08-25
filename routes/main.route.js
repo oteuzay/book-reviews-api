7604932f63b338f528470779804e8bfc7eca85d1
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-25 21:55:08 +0300

    feat: add authorsRoute to main

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoute from "./auth.route.js";
+import authorsRoute from "./authors.route.js";
 import swaggerRoute from "./swagger.route.js";
 
 const router = Router();
 
 router.use("/api-docs", swaggerRoute);
 router.use("/auth", authRoute);
+router.use("/authors", authorsRoute);
 
 export default router;
