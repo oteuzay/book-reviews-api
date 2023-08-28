4a28eebb703c3d1b36ec973d57071681ba3091b4
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-29 00:28:57 +0300

    feat: add swagger doc. to authors route

--- routes/authors.route.js
+++ routes/authors.route.js
@@ -57,6 +57,38 @@ router.get(
   authorsController.getAuthor
 );
 
+/**
+ * @swagger
+ * /api/authors:
+ *   post:
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               surname:
+ *                  type: string
+ *               summary:
+ *                  type: string
+ *             example:
+ *               name: Ursula
+ *               surname: Le Guin
+ *               summary: Ursula K Le Guin is an American author who has written novels, children's books, poetry, short stories and essays.
+ *     responses:
+ *       201:
+ *         description: Success
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post(
   "/",
   authCheck,
@@ -64,6 +96,45 @@ router.post(
   authorsController.createAuthor
 );
 
+/**
+ * @swagger
+ * /api/authors/{authorID}:
+ *   put:
+ *     summary:
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: authorID
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               surname:
+ *                  type: string
+ *               summary:
+ *                  type: string
+ *             example:
+ *               name: Ursula
+ *               surname: Le Guin, Kroeber
+ *               summary: Ursula K Le Guin is an American author who has written novels, children's books, poetry, short stories and essays. Most of her work has been written within the fantasy and science fiction genres and she has won many awards for her work, including five Hugo Awards, 6 Nebula Awards, 19!
+ *     responses:
+ *       200:
+ *         description: Success
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.put(
   "/:authorID",
   authCheck,
@@ -71,6 +142,30 @@ router.put(
   authorsController.updateAuthor
 );
 
+/**
+ * @swagger
+ * /api/authors/{authorID}:
+ *   delete:
+ *     summary:
+ *     tags: [Authors]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: authorID
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.delete(
   "/:authorID",
   authCheck,
