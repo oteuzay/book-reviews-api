ff16f7f68c24b5da342ec3292eb0ae2fbb161078
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-28 22:38:02 +0300

    feat: add swagger doc. to auth route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,145 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ */
 import express from "express";
 import authController from "../controllers/auth.controller.js";
 import { authValidator } from "../validators/auth.validator.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Sign up
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: username
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: user@website.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: 123456
+ *     responses:
+ *       201:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       422:
+ *         description: Unprocessable Entity
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post("/signup", authValidator.signUp, authController.signUp);
+
+/**
+ * @swagger
+ * /api/auth/signin:
+ *   post:
+ *     summary: Sign in
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: user@website.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post("/signin", authValidator.signIn, authController.signIn);
+
+/**
+ * @swagger
+ * /api/auth/signout:
+ *   post:
+ *     summary: Sign out
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 format: refreshToken
+ *                 example: ""
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post("/signout", authValidator.signOut, authController.signOut);
+
+/**
+ * @swagger
+ * /api/auth/refreshToken:
+ *   post:
+ *     summary: Refresh Token
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 format: refreshToken
+ *                 example: ""
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post(
   "/refreshToken",
   authValidator.refreshToken,
