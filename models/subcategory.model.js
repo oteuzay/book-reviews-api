e0dbfe683a9ba60cb353d97760d4d1458d66ae2c
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-30 19:58:43 +0300

    feat: create a model for subcategory

---
+++ models/subcategory.model.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../database/index.database.js";
+
+import Category from "./category.model.js";
+
+const Subcategory = sequelize.define(
+  "Subcategory",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },
+  {
+    timestamps: true,
+    tableName: "subcategory",
+  }
+);
+
+Subcategory.belongsTo(Category, {
+  foreignKey: "category_id",
+});
+
+export default Subcategory;
