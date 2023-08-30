8816fd0d59e0af3750d2850707ae92811e1c5b51
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-30 20:01:34 +0300

    feat: add route of subcategories to main route

--- routes/main.route.js
+++ routes/main.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRoute from "./auth.route.js";
 import authorsRoute from "./authors.route.js";
 import categoriesRoute from "./categories.route.js";
+import subcategoriesRoute from "./subcategories.route.js";
 import swaggerRoute from "./swagger.route.js";
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 router.use("/auth", authRoute);
 router.use("/authors", authorsRoute);
 router.use("/categories", categoriesRoute);
+router.use("/subcategories", subcategoriesRoute);
 router.use("/api-docs", swaggerRoute);
 
 export default router;
