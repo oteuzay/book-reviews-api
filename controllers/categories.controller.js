45c073e371ff2182b3543723e1a72a70ff1af76b
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-01 17:35:07 +0300

    feat: add new controllers

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -45,8 +45,54 @@ class CategoriesController {
     }
   }
 
-  async updateCategory(req, res, next) {}
-  async deleteCategory(req, res, next) {}
+  /**
+   * The `updateCategory` function updates a category with the provided category ID and user ID, and
+   * returns the updated category's ID and title in the response.
+   */
+  async updateCategory(req, res, next) {
+    try {
+      const userID = req.payload.aud;
+      const categoryID = req.params.categoryID;
+      const category = {
+        title: req.body.title,
+      };
+
+      const { id, title } = await categoriesService.updateCategory(
+        userID,
+        categoryID,
+        category
+      );
+
+      res.status(200).json({
+        message: "Category successfully updated.",
+        category: {
+          id: id,
+          title: title,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  /**
+   * The `deleteCategory` function deletes a category based on the category ID provided in
+   * the request parameters.
+   */
+  async deleteCategory(req, res, next) {
+    try {
+      const userID = req.payload.aud;
+      const categoryID = req.params.categoryID;
+
+      await categoriesService.deleteCategory(userID, categoryID);
+
+      res.status(200).json({
+        message: "Category successfully deleted.",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default new CategoriesController();
