82302d3a726f832480e7120d29d324aa8613c40b
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-07 02:41:10 +0300

    feat: implement `sort_order` and refactor code base

--- services/categories.service.js
+++ services/categories.service.js
@@ -1,23 +1,32 @@
 import createError from "http-errors";
 import usersService from "./users.service.js";
+import subcategoriesService from "./subcategories.service.js";
 import categoriesRepository from "../repositories/category.repository.js";
 
 class CategoriesService {
   /**
-   * The function `getCategories` retrieves a list of categories and their count from a repository and
-   * returns them in a specific format.
+   * The function "countCategories" asynchronously retrieves the count of categories from the categories repository.
+   * @returns The count of categories.
+   */
+  async countCategories() {
+    const countCategories = await categoriesRepository.countCategories();
+    return countCategories;
+  }
+
+  /**
+   * The function `getCategories` retrieves categories and their statistics from a repository.
    * @returns an object with two properties: `categories` and `stats`.
    */
   async getCategories() {
-    const countCategories = await categoriesRepository.countCategories();
+    const countCategories = await this.countCategories();
     const categories = await categoriesRepository.getCategories();
 
-    // TODO: Perform the 'map' operation in the service layer for better separation of concerns.
     return {
       categories: categories.map((category) => {
         return {
           id: category.id,
           title: category.title,
+          sort_order: category.sort_order,
         };
       }),
       stats: {
@@ -28,9 +37,9 @@ class CategoriesService {
 
   /**
    * The function `getCategory` retrieves a category based on its ID and throws an error if the
-   * category is not found.
-   * @param categoryID - The categoryID parameter is the unique identifier of the category that we want
-   * to retrieve from the categories repository.
+   * category is not found or has been deleted.
+   * @param categoryID - The categoryID parameter is the unique identifier of the category that you
+   * want to retrieve.
    * @returns The category object is being returned.
    */
   async getCategory(categoryID) {
@@ -40,14 +49,20 @@ class CategoriesService {
       throw createError.NotFound("The requested category was not found.");
     }
 
+    if (category.status == false) {
+      throw createError.BadRequest("This category has already been deleted or does not exist.");
+    }
+
     return category;
   }
 
   /**
-   * The function creates a category if the user is an admin and returns the created category's id and title.
+   * The function creates a category and returns its id, title, and sort order.
    * @param userID - The userID parameter is the unique identifier of the user who is creating the category.
-   * @param category - The `category` parameter represents the category object that you want to create.
-   * @returns an object with the properties "id" and "title".
+   * @param category - The `category` parameter is an object that represents the category to be
+   * created. It typically contains properties such as `title` (the title of the category) and
+   * `sort_order` (the sorting order of the category).
+   * @returns an object with the properties "id", "title", and "sort_order".
    */
   async createCategory(userID, category) {
     await usersService.isUserAdmin(userID);
@@ -57,15 +72,19 @@ class CategoriesService {
     return {
       id: createdCategory.id,
       title: createdCategory.title,
+      sort_order: createdCategory.sort_order,
     };
   }
 
   /**
-   * The function updates a category and returns the updated category's ID and title.
+   * The function updates a category by calling other functions to check user admin status, retrieve
+   * the category, and update the category in the repository, and then returns the updated category's
+   * id, title, and sort order.
    * @param userID - The userID parameter represents the ID of the user who is performing the category update.
    * @param categoryID - The category ID is a unique identifier for the category that needs to be updated.
-   * @param category - The `category` parameter is an object that represents the updated category information.
-   * @returns an object with the properties "id" and "title" of the updated category.
+   * @param category - The `category` parameter is an object that represents the updated category
+   * information. It typically includes properties such as `id`, `title`, and `sort_order`.
+   * @returns an object with the properties "id", "title", and "sort_order".
    */
   async updateCategory(userID, categoryID, category) {
     await usersService.isUserAdmin(userID);
@@ -76,11 +95,13 @@ class CategoriesService {
     return {
       id: updatedCategory.id,
       title: updatedCategory.title,
+      sort_order: updatedCategory.sort_order,
     };
   }
 
   /**
-   * The function `deleteCategory` deletes a category if it exists and the user is an admin.
+   * The function `deleteCategory` deletes a category if the user is an admin and the category does not
+   * contain any subcategories.
    * @param userID - The userID parameter represents the ID of the user who is trying to delete the category.
    * @param categoryID - The categoryID parameter is the unique identifier of the category that needs to be deleted.
    * @returns Nothing is being returned.
@@ -88,12 +109,13 @@ class CategoriesService {
   async deleteCategory(userID, categoryID) {
     await usersService.isUserAdmin(userID);
     const category = await this.getCategory(categoryID);
+    const countSubcategories = await subcategoriesService.countSubcategories(categoryID);
 
-    if (category.status == false) {
-      throw createError.BadRequest("This category has already been deleted or does not exist.");
+    if (countSubcategories !== 0) {
+      throw createError.BadRequest(
+        "This category cannot be deleted because it contains subcategories."
+      );
     }
-
-    // TODO: Check if the category has any subcategories; if not, proceed with deletion.
 
     await categoriesRepository.deleteCategory(category);
 
