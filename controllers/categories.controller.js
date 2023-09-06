670cc5c96966009199d9dc1fed8072e87891b774
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-07 02:28:27 +0300

    feat: implement `sort_order`

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -18,23 +18,25 @@ class CategoriesController {
   }
 
   /**
-   * The function creates a category with a title provided in the request body and returns the ID and
-   * title of the created category.
+   * The `createCategory` function is an asynchronous function that creates a new category with the
+   * provided title and sort order and returns the created category's ID, title, and sort order.
    */
   async createCategory(req, res, next) {
     try {
       const userID = req.payload.aud;
       const category = {
         title: req.body.title,
+        sort_order: req.body.sort_order,
       };
 
-      const { id, title } = await categoriesService.createCategory(userID, category);
+      const { id, title, sort_order } = await categoriesService.createCategory(userID, category);
 
       res.status(201).json({
         message: "Category successfully created.",
         category: {
           id: id,
           title: title,
+          sort_order: sort_order,
         },
       });
     } catch (error) {
@@ -43,8 +45,8 @@ class CategoriesController {
   }
 
   /**
-   * The `updateCategory` function updates a category with the provided category ID and user ID, and
-   * returns the updated category's ID and title in the response.
+   * The function `updateCategory` updates a category with the provided category ID and user ID, and
+   * returns the updated category information.
    */
   async updateCategory(req, res, next) {
     try {
@@ -52,15 +54,21 @@ class CategoriesController {
       const categoryID = req.params.categoryID;
       const category = {
         title: req.body.title,
+        sort_order: req.body.sort_order,
       };
 
-      const { id, title } = await categoriesService.updateCategory(userID, categoryID, category);
+      const { id, title, sort_order } = await categoriesService.updateCategory(
+        userID,
+        categoryID,
+        category
+      );
 
       res.status(200).json({
         message: "Category successfully updated.",
         category: {
           id: id,
           title: title,
+          sort_order: sort_order,
         },
       });
     } catch (error) {
