be5fdddada0c11d05992a0b71ae9c84d461c6d44
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-07 03:21:12 +0300

    feat: add users and subcategories routes

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import authRoute from "./auth.route.js";
+import usersRoute from "./users.route.js";
 import authorsRoute from "./authors.route.js";
 import categoriesRoute from "./categories.route.js";
+import subcategoriesRoute from "./subcategories.route.js";
 import swaggerRoute from "./swagger.route.js";
 
 const router = Router();
 
 router.use("/auth", authRoute);
+router.use("/users", usersRoute);
 router.use("/authors", authorsRoute);
 router.use("/categories", categoriesRoute);
+router.use("/subcategories", subcategoriesRoute);
 router.use("/api-docs", swaggerRoute);
 
 export default router;
