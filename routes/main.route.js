e2868b2cf47ca907efee97b29403fa89016afe5b
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-11 19:43:48 +0300

    fix: delete old route of subcategories

--- routes/main.route.js
+++ routes/main.route.js
@@ -3,7 +3,6 @@ import authRoute from "./auth.route.js";
 import usersRoute from "./users.route.js";
 import authorsRoute from "./authors.route.js";
 import categoriesRoute from "./categories.route.js";
-import subcategoriesRoute from "./subcategories.route.js";
 import swaggerRoute from "./swagger.route.js";
 
 const router = Router();
@@ -12,7 +11,6 @@ router.use("/auth", authRoute);
 router.use("/users", usersRoute);
 router.use("/authors", authorsRoute);
 router.use("/categories", categoriesRoute);
-router.use("/subcategories", subcategoriesRoute);
 router.use("/api-docs", swaggerRoute);
 
 export default router;
