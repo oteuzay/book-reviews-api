75c52d0d30c0461b9b04ee8a963077b2a1719b3f
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-13 11:50:18 +0300

    feat: add missing validators

--- validators/subcategories.validator.js
+++ validators/subcategories.validator.js
@@ -3,17 +3,69 @@ import { validationCheck } from "../middleware/validation-check.middleware.js";
 
 const authValidator = {
   getSubcategories: [
-    param("categoryID").isUUID().withMessage("categoryID must be in UUID format."),
+    param("categoryID")
+      .trim()
+      .notEmpty()
+      .withMessage("categoryID is required.")
+      .isUUID()
+      .withMessage("categoryID must be in UUID format."),
     validationCheck,
   ],
-  createSubcategories: [
+  createSubcategory: [
     body("title")
       .trim()
       .notEmpty()
       .withMessage("Title is required.")
       .isLength({ min: 5 })
       .withMessage("Title must be at least 5 characters long."),
-    param("categoryID").isUUID().withMessage("categoryID must be in UUID format."),
+    body("sort_order")
+      .trim()
+      .notEmpty()
+      .withMessage("sort_order is required.")
+      .isInt()
+      .withMessage("sort_order must be in Int format."),
+    param("categoryID")
+      .trim()
+      .notEmpty()
+      .withMessage("categoryID is required.")
+      .isUUID()
+      .withMessage("categoryID must be in UUID format."),
+    validationCheck,
+  ],
+  updateSubcategory: [
+    body("title")
+      .trim()
+      .notEmpty()
+      .withMessage("Title is required.")
+      .isLength({ min: 5 })
+      .withMessage("Title must be at least 5 characters long."),
+    body("sort_order")
+      .trim()
+      .notEmpty()
+      .withMessage("sort_order is required.")
+      .isInt()
+      .withMessage("sort_order must be in Int format."),
+    body("categoryID")
+      .trim()
+      .notEmpty()
+      .withMessage("categoryID is required.")
+      .isUUID()
+      .withMessage("categoryID must be in UUID format."),
+    param("subcategoryID")
+      .trim()
+      .notEmpty()
+      .withMessage("subcategoryID is required.")
+      .isUUID()
+      .withMessage("subcategoryID must be in UUID format."),
+    validationCheck,
+  ],
+  deleteSubcategory: [
+    param("subcategoryID")
+      .trim()
+      .notEmpty()
+      .withMessage("subcategoryID is required.")
+      .isUUID()
+      .withMessage("subcategoryID must be in UUID format."),
     validationCheck,
   ],
 };
